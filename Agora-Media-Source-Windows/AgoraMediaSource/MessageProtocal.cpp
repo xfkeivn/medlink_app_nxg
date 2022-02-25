960618e9a5600aecb4d6525b82f9ded858bfa6fe
Zha <Xiuqi.Zha@bsci.com>
2022-02-25 17:36:26 +0800

    remove split method as it has been added to commonFun file, remove websocketclient class
    Signed-off-by: Zha <Xiuqi.Zha@bsci.com>

--- Agora-Media-Source-Windows/AgoraMediaSource/MessageProtocal.cpp
+++ Agora-Media-Source-Windows/AgoraMediaSource/MessageProtocal.cpp
@@ -21,17 +21,6 @@ void RTMMessagEvent::fromRTMMessage(UINT uid, string &message)
 }
 
 
-static void split(const string& s, vector<string>& tokens, const char& delim = ' ') {
-	tokens.clear();
-	size_t lastPos = s.find_first_not_of(delim, 0);
-	size_t pos = s.find(delim, lastPos);
-	while (lastPos != string::npos) {
-		tokens.emplace_back(s.substr(lastPos, pos - lastPos));
-		lastPos = s.find_first_not_of(delim, pos);
-		pos = s.find(delim, lastPos);
-	}
-}
-
 
 string packetMouseEvent(MSG_EVENT_TYPE mousetype, int screen_w, int screen_h, int x, int y, int mouseStatus)
 {
